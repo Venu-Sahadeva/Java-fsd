7efe6d10611c072771b6a4d9224b0ad3722eb3ca
Venu-Sahadeva <venusahadeva@gmail.com>
2023-12-07 00:34:51 +0530

    message

---
+++ TicketBookingApp/src/com/ticketbooking/main/App.java
@@ -0,0 +1,15 @@
+package com.ticketbooking.main;
+
+import com.ticketbooking.service.MovieShowService;
+
+public class App {
+
+	public static void main(String[] args) {
+			MovieShowService ms = new MovieShowService();
+			//ms.showMoviewAndSeatInfo();
+			ms.searchMoviInfoByDate("7-Dec");
+			}	
+
+	}
+
+
